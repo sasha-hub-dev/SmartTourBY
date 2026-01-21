c9d7de18fe8be67b267746c568111fc49f82003d
sasha-hub-dev <sashabuden2007@gmail.com>
2026-01-21 12:10:37 +0300

    "feat(auth): full auth flow working with JWT and Swagger"

--- backend/auth-service/src/main/java/by/smarttour/auth/config/JwtService.java
+++ backend/auth-service/src/main/java/by/smarttour/auth/config/JwtService.java
@@ -2,7 +2,6 @@ package by.smarttour.auth.config;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
