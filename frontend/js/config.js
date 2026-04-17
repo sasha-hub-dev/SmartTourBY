689d8b3130d92cfa41bbe010311817c58b587b64
sasha-hub-dev <sashabuden2007@gmail.com>
2026-04-17 19:26:27 +0300

    arch: create project structure for SaaS historical tourism service

---
+++ frontend/js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    TOUR_SERVICE: 'http://localhost:8082/api/v1',
+    BOOKING_SERVICE: 'http://localhost:8086/api/v1',
+    AUTH_SERVICE: 'http://localhost:8080/api/v1/auth'
+};
+
+export default CONFIG;
